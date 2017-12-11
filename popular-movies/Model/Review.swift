0eb4257b39f19bcc64cdc7f5cc3a1f23e6e826da
Robihamanto <Robihamanto@Robihamantos-MacBook-Pro.local>
2017-12-11 08:44:44 +0700

    added choose category

---
+++ popular-movies/Model/Review.swift
@@ -0,0 +1,9 @@
+//
+//  Review.swift
+//  popular-movies
+//
+//  Created by Robihamanto on 10/12/17.
+//  Copyright © 2017 Robihamanto. All rights reserved.
+//
+
+import Foundation
---
+++ popular-movies/View/ReviewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewCell.swift
+//  popular-movies
+//
+//  Created by Robihamanto on 10/12/17.
+//  Copyright © 2017 Robihamanto. All rights reserved.
+//
+
+import UIKit
+
+class ReviewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
